a80ebdc9c0c6ddc3aa22635a697ce2e137cffbde
Ilya Portnov <portnov@bk.ru>
2019-08-17 12:19:28 +0500

    Manual parsing on lazy text.

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ module Main where
 import Control.Monad
 import Control.Applicative
 import qualified Data.Text.IO as TIO
+import qualified Data.Text.Lazy.IO as TLIO
 import Data.Attoparsec.Text
 import System.Environment
 
@@ -15,18 +16,10 @@ import Operations
 main :: IO ()
 main = do
   [file] <- getArgs
-  text <- TIO.readFile file
---   case parseOnly pSample text of
---     Left err -> fail err
---     Right sample -> print sample
---   case parseOnly (many pSample) text of
---     Left err -> fail err
---     Right sample -> print sample
-  case parseOnly pHeap text of
-    Left err -> fail err
-    Right heap -> do
-      print $ heapHeader heap
-      print $ length $ heapSamples heap
+  text <- TLIO.readFile file
+  let heap = parseHeap text
+  print $ heapHeader heap
+  print $ length $ heapSamples heap
 
-      runWindow heap
+  -- runWindow heap
 
--- src/Parser.hs
+++ src/Parser.hs
@@ -3,42 +3,124 @@
 module Parser where
 
 import Control.Monad
+import Control.Monad.State
 import Control.Applicative
 import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
 import qualified Data.Map as M
-import Data.Attoparsec.Text
+import Data.Attoparsec.Text.Lazy
 
 import Types
 
-pString :: Parser T.Text
+data ParserState = ParserState {
+    psHeader :: Header
+  , psHeaderParsed :: Bool
+  , psSamples :: [Sample ItemsMap]
+  , psCurrentSample :: Sample ItemsMap
+  }
+
+data LineData =
+    JOB TL.Text
+  | DATE TL.Text
+  | SAMPLE_UNIT TL.Text
+  | VALUE_UNIT TL.Text
+  | BEGIN_SAMPLE Double
+  | END_SAMPLE Double
+  | DATA TL.Text Int
+  deriving (Show)
+
+initState :: ParserState
+initState = ParserState zeroHeader False [] undefined
+
+zeroHeader :: Header
+zeroHeader = Header "" "" "" ""
+
+parseHeap :: TL.Text -> Heap
+parseHeap text =
+    let st = execState runParser initState
+        runParser = forM_ (TL.lines text) parseLine
+    in  Heap (psHeader st) (reverse $ psSamples st)
+
+processLine :: LineData -> State ParserState ()
+processLine (JOB job) = modify $ \st -> st {psHeader = (psHeader st) {hJob = TL.toStrict job}}
+processLine (DATE date) = modify $ \st -> st {psHeader = (psHeader st) {hDate = TL.toStrict date}}
+processLine (SAMPLE_UNIT unit) = modify $ \st -> st {psHeader = (psHeader st) {hSampleUnit = TL.toStrict unit}}
+processLine (VALUE_UNIT unit) =
+  modify $ \st -> st {
+        psHeader = (psHeader st) {hValueUnit = TL.toStrict unit}
+      , psHeaderParsed = True
+    }
+processLine (BEGIN_SAMPLE time) =
+  modify $ \st -> st {psCurrentSample = Sample time M.empty}
+processLine (END_SAMPLE _) =
+  modify $ \st -> st {psSamples = psCurrentSample st : psSamples st}
+processLine (DATA name value) =
+  modify $ \st -> st {
+      psCurrentSample =
+          let items = sampleItems (psCurrentSample st)
+          in  (psCurrentSample st) {sampleItems = M.insert (TL.toStrict name) value items}
+    }
+
+begin_sample :: TL.Text
+begin_sample = "BEGIN_SAMPLE"
+
+end_sample :: TL.Text
+end_sample = "END_SAMPLE"
+
+parseLine :: TL.Text -> State ParserState ()
+parseLine line = do
+    st <- get
+    if not (psHeaderParsed st)
+      then do
+        case eitherResult $ parse pHeaderLine line of
+          Left err -> fail $ "Can't parse header line: " ++ show line ++ ": " ++ err
+          Right lineData -> processLine lineData
+      else 
+        if begin_sample `TL.isPrefixOf` line
+          then processLine =<< BEGIN_SAMPLE <$> parseSample begin_sample line
+          else if end_sample `TL.isPrefixOf` line
+                 then processLine =<< END_SAMPLE <$> parseSample end_sample line
+                 else do
+                  (name, value) <- parseItem line
+                  processLine $ DATA name value
+
+-- | Parse BEGIN_SAMPLE or END_SAMPLE line
+parseSample :: Monad m => TL.Text -> TL.Text -> m Double
+parseSample name line = do
+  let rest = TL.drop (TL.length name + 1) line
+  case eitherResult $ parse pDouble rest of
+    Left err -> fail $ "Can't parse double: " ++ show rest ++ ": " ++ err
+    Right value -> return value
+
+-- | Parse data item line
+parseItem :: Monad m => TL.Text -> m (TL.Text, Int)
+parseItem line = do
+  let (name, rest) = TL.break (== '\t') line
+  if TL.null rest
+    then fail $ "Can't parse data line: " ++ show line
+    else case eitherResult $ parse decimal (TL.tail rest) of
+           Left err -> fail $ "can't parse decimal: " ++ show rest ++ ": " ++ err
+           Right value -> return (name, value)
+    
+pHeaderLine :: Parser LineData
+pHeaderLine =
+      (JOB <$> pStringValue "JOB")
+  <|> (DATE <$> pStringValue "DATE")
+  <|> (SAMPLE_UNIT <$> pStringValue "SAMPLE_UNIT")
+  <|> (VALUE_UNIT <$> pStringValue "VALUE_UNIT")
+
+pString :: Parser TL.Text
 pString = do  
   char '"'
   string <- manyTill anyChar $ char '"'
-  return $ T.pack string
+  return $ TL.pack string
 
-pStringValue :: T.Text -> Parser T.Text
+pStringValue :: T.Text -> Parser TL.Text
 pStringValue name = do
   string name
   space
   value <- pString
-  endOfLine
   return value
-
-pHeader :: Parser Header
-pHeader = Header
-  <$> pStringValue "JOB"
-  <*> pStringValue "DATE"
-  <*> pStringValue "SAMPLE_UNIT"
-  <*> pStringValue "VALUE_UNIT"
-
-pItem :: Parser (T.Text, Int)
-pItem = do
-  name <- manyTill anyChar $ char '\t'
-  let nameT = T.pack name
-  guard $ not $ "END_SAMPLE" `T.isPrefixOf` nameT
-  value <- decimal
-  endOfLine
-  return (nameT, value)
 
 -- for some reason, Haskell programs sometimes use
 -- comma as decimal separator instead of dot.
@@ -53,23 +135,4 @@ pDouble = doubleComma <|> double
       mantis <- decimal
       let n = length zeros + length (show mantis)
       return $ fromIntegral int + fromIntegral mantis / 10^n
-
-pSample :: Parser (Sample ItemsMap)
-pSample = do
-  string "BEGIN_SAMPLE"
-  space
-  time <- pDouble
-  endOfLine
-  items <- many pItem
-  string "END_SAMPLE"
-  space
-  time1 <- pDouble
-  guard $ time1 == time
-  endOfLine
-  return $ Sample time (M.fromList items)
-
-pHeap :: Parser Heap
-pHeap = Heap
-  <$> pHeader
-  <*> many pSample
 
